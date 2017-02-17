909e6f983f88e0868841c72b70c5420b2919fb69
SUZUKIyuka <yuka.suzuki1218@gmail.com>
2017-02-17 20:22:31 +0900

    Initial Commit

---
+++ ClipImageSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClipImageSample
+//
+//  Created by nagata on 2/17/17.
+//  Copyright © 2017 nagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
